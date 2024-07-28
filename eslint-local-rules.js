670b1b6c18044f130730bf6785c7036e5168552a
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-07-28 12:28:57 +0200

    chore: project skeleton

---
+++ eslint-local-rules.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// See https://github.com/cletusw/eslint-plugin-local-rules
+module.exports = {
+  'no-render-string-reference': require('./scripts/eslint-rules/no-render-string-reference'),
+  'no-components-index': require('./scripts/eslint-rules/no-components-index'),
+  'jsx-condition-key': require('./scripts/eslint-rules/jsx-condition-key'),
+  'jsx-curly-spacing': require('./scripts/eslint-rules/jsx-curly-spacing'),
+  'jest-global-imports': require('./scripts/eslint-rules/jest-global-imports'),
+  'cypress-types-reference': require('./scripts/eslint-rules/cypress-types-reference'),
+  'sort-imports': require('./scripts/eslint-rules/sort-imports'),
+  'no-nullish-coalescing-in-condition': require('./scripts/eslint-rules/no-nullish-coalescing-in-condition'),
+}
---
+++ packages/README.md
@@ -0,0 +1,8 @@
+<p>These are the packages for the Alpinui monorepo.</p>
+
+## Packages for Alpinui
+
+1. **alpine-composition** - Vue [composition API](https://vuejs.org/guide/extras/composition-api-faq.html) system for AlpineJS.
+2. **alpine-provide-inject** - Vue [provide/inject](https://vuejs.org/guide/components/provide-inject) system for AlpineJS.
+3. **alpine-reactivity** - Vue [reactivity](https://github.com/vuejs/core/tree/main/packages/reactivity#readme) system for AlpineJS.
+4. **alpinui** - main source code for Alpinui - framework, components, styles, etc.
